0e3ef7d3e60411cb209438092e84406da1589e2f
Maulana641 <62836628+Maulana641@users.noreply.github.com>
2020-04-20 19:00:54 +0800

    Add files via upload

---
+++ TUGAS ALPRO2 PEMROSESAN FILE PT 1 5.2 .cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <conio.h>
+#include <fstream>
+using namespace std;
+int main()
+
+{
+
+ofstream file_keluaran;
+
+file_keluaran.open("Saya belajar membuat file.TXT");
+
+file_keluaran;
+
+cout<<"Saya belajar membuat file"<<endl;
+
+return 0;
